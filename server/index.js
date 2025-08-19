a522cb6b6881e14356024fd12ad446fbf02855e9
Chanh Tran <chanh.tran@redis.com>
2025-08-19 16:16:25 -0500

    initial commit

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ import express from "express";
 import * as api from "./router.js";
 import { initialize } from "./app.js";
 
-// await initialize();
+await initialize();
 
 const app = express();
 
 app.use(express.static("public"));
 
-// app.use(express.json());
-// app.use("/api", api.router);
+app.use(express.json());
+app.use("/api", api.router);
 
 app.listen(3000, async () => {
   console.log(`Server listening on port 3000`);
